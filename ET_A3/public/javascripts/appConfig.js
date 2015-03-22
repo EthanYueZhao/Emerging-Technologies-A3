f79c991965e8ea59b69471152e659680b20d5046
Ethan Yue Zhao <xishanlake@gmail.com>
2015-03-22 00:00:24 -0400

    added search service, but it cannot pass value

--- ET_A3/public/javascripts/appConfig.js
+++ ET_A3/public/javascripts/appConfig.js
@@ -3,39 +3,37 @@
 // main app module
 var a3App = angular.module('a3App', ['ngRoute', 'ui.bootstrap', 'DataServices']);
 
-a3App.config(['$routeProvider',
-    function ($routeProvider) {
+a3App.config(['$routeProvider', function ($routeProvider) {
+        
         $routeProvider.
-      when('/', {
+        when('/', {
             templateUrl: 'partials/listView.html',
             controller: 'patientListCtrl'
         }).
-     
-      otherwise({
+        otherwise({
             redirectTo: '/'
         });
-    }]);
+    }
+]);
 
 // all data services
-
 var DataServices = angular.module('DataServices', ['ngResource']);
 
 // in charge of patients data
-DataServices.factory('patientsDS', ['$resource', 
-    function ($resource) {
-        return $resource('patients/:id', null, { 'update': { method: 'PUT' } });
-    }]);
+DataServices.factory('patientsDS', function ($resource) {
+    return $resource('patients/:id', null, { 'update': { method: 'PUT' } });
+});
 
 // in charge of doctors data
-DataServices.factory('doctorsDS', ['$resource', 
-    function ($resource) {
-        return $resource('doctors/:id', null, { 'update': { method: 'PUT' } });
-    }]);
+DataServices.factory('doctorsDS', function ($resource) {
+    return $resource('doctors/:id', null, { 'update': { method: 'PUT' } });
+});
 
-// in charge of issuses
-//DataServices.factory('IssueDS', ['$resource', 
-//    function ($resource)
-//    {
-//        return $resource('IssueAPI/:id', null, { 'update': { method: 'PUT' } });
-
-//    }]);
+// in charge of search parameters
+DataServices.factory('searchDS', function () {
+    var patients = {};
+    return {
+        getList: function () { return patients; },        
+        setList: function (list) { patients = list; }
+    };
+});
--- ET_A3/public/javascripts/controllers.js
+++ ET_A3/public/javascripts/controllers.js
@@ -1,8 +1,29 @@
 ﻿'use strict';
 
-var patientListCtrl = a3App.controller('patientListCtrl', ['$scope', 'patientsDS', 
-    function ($scope, patientsDS) {
-        $scope.patients = patientsDS.query();
-    }
-]);
+var patientListCtrl = a3App.controller('patientListCtrl', function ($scope, patientsDS, searchDS) {
+    //var list = searchDS.getList();
+    //console.log('list+++++' + list);
+    
+    //if (list != null)
+    //{
+    //    $scope.patients = list;
+    //}
+    $scope.$watch(function () { return searchDS.getList(); });
+    console.log('listCtrl+++++' );
+    console.log(searchDS.getList());
+    $scope.patients = searchDS.getList();
+   // $scope.patients = patientsDS.query();
+        
+});
+
+var navbarCtrl = a3App.controller('navbarCtrl', function ($scope, patientsDS, searchDS) {
+    $scope.searchByLastName = function () {
+        //searchDS.searchPara.para = $scope.lastName;
+        //console.log(searchDS.searchPara.para); 
+        searchDS.setList(patientsDS.query());
+        console.log('navbarCtrl+++++' + searchDS.getList());
+        console.log(searchDS.getList());
+    };
+   
+});
 
